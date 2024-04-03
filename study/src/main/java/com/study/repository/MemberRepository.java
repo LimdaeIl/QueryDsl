ac2792c0ef3ba286b21b5dd3e7682ff4aed461c1
LimDae94 <piay801@naver.com>
2024-04-03 15:59:18 +0900

    querydsl 페이징

---
+++ study/src/main/java/com/study/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.study.repository;
+
+import com.study.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+
+    List<Member> findByUsername(String username);
+}
---
+++ study/src/main/java/com/study/repository/MemberRepositoryCustom.java
@@ -0,0 +1,16 @@
+package com.study.repository;
+
+import com.study.dto.MemberSearchCondition;
+import com.study.dto.MemberTeamDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+
+    List<MemberTeamDto> search(MemberSearchCondition condition);
+
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
+}
